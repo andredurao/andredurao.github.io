4d03245d2b46f3d6028f34f3efb49a9ec5d364ed
André Durão <andreluisdurao@gmail.com>
2019-09-16 19:27:56 -0300

    Add first sample cheat sheets

--- script.js
+++ script.js
@@ -46,17 +46,27 @@ let files = {
     Please review my CV, which provides details and examples of skills in:
 
     * Backend
-      Ruby, Ruby On Rails, Node.js, Java
+      Ruby, Ruby On Rails, Node.js, Java, Go
     * Frontend
-      HTML, JS, CSS, Vue.js, React, Bootstrap, JQuery
+      HTML, JS, CSS, Vue.js, React, Bootstrap
     * Databases
       PostgreSQL, MySQL / MariaDB, MongoDB 
     * Devops
-      AWS, EC2, EB, Docker
+      AWS, EC2, Docker, Kubernetes
     * Methodologies
       Agile, Scrum, TDD, BDD`,
-  'cv.md': `
-    `
+  'cv.md': ``,
+  'bash-cheat-sheet.md': `
+    * Redirect find error messages to null:
+      find . -name 'some-name' 2 > /dev/null
+  `,
+  'docker-cheat-sheet.md': `
+    * Prune [DELETE] entire system ☠️⚠️:
+      docker stop $(docker ps -a -q) && docker system prune -a -f --volumes
+    * Search containers by their CPU Usage:
+      docker stats --no-stream --format \
+ "table {{.Name}}\\t{{.Container}}\\t{{.CPUPerc}}\t{{.MemUsage}}" | \
+ sort -k 3 -h -r | head`,
 }
 
 let funcs = {
