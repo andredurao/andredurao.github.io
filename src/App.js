132948ee1f6de85bb0be3340d417c4597941ee71
André Durão <andreluisdurao@gmail.com>
2020-07-30 23:24:42 -0300

    WIP Start onKeyDown handler

--- src/App.js
+++ src/App.js
@@ -1,38 +1,11 @@
 import React, { useState } from 'react';
 import Terminal from './Terminal';
-import terminalParse from './TerminalFunctions';
 import './App.css';
 
 function App() {
-  const [terminalContent, setTerminalContent] = useState('');
-  const handleKeyDown = event => {
-    if (event.key === 'Enter') {
-      console.log('Enter');
-      debugger;
-      terminalParse(terminalContent);
-      // run();
-      setTerminalContent("test");
-    } else if (event.key === 'ArrowUp') {
-      console.log('ArrowUp');
-      // if (hi <= history.length && hi > 0) {
-      //   $prompt.value = history[--hi];
-      // }
-    } else if (event.key === 'ArrowDown') {
-      console.log('ArrowDown');
-      // if (hi + 1 < history.length && hi >= 0) {
-      //   $prompt.value = history[++hi];
-      // }
-    } else if (event.key === 'Tab') {
-      console.log('Tab');
-      event.preventDefault();
-      // $prompt.value = tabComplete($prompt.value);
-    } else {
-      // hi = history.length;
-    }
-  };
   return (
     <div className="App">
-      <Terminal content={terminalContent} onKeyDownTerminal={handleKeyDown} />
+      <Terminal />
     </div>
   );
 }
--- src/Terminal.js
+++ src/Terminal.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Terminal.css';
 import Content from './Content.js';
+import terminalParse from './TerminalFunctions';
 
-function Terminal({ content, onKeyDownTerminal }) {
+function Terminal() {
+  const [command, setCommand] = useState('');
+
+  const handleChange = event => {
+    setCommand(event.target.value);
+  };
+
+  const handleKeyDown = event => {
+    if (event.key === 'Enter') {
+      terminalParse(command);
+    } else if (event.key === 'ArrowUp') {
+      // setCommand('');
+      // seek history up
+    } else if (event.key === 'ArrowDown') {
+      // seek history down
+    } else if (event.key === 'Tab') {
+      // complete
+      event.preventDefault();
+    } else {
+      // reset historyIndex
+    }
+  };
   return (
     <div className='Terminal'>
-      <Content content={content} />
+      <Content content='' />
       <div className='input'>
         <span>~ $</span>
-        <input id='prompt' type='text' autoComplete='off' spellCheck='false' onKeyDown={onKeyDownTerminal}>
+        <input id='prompt' type='text' autoComplete='off' spellCheck='false' onKeyDown={handleKeyDown} onChange={handleChange} value={command}>
         </input>
       </div>
       <div id='key' className='hidden'>⏎</div>
